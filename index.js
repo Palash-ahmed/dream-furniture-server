84ddddf7c9008eb29eb1858b952d509db2707b5c
Palash Ahmed <palashbiochem@gmail.com>
2022-11-24 19:02:35 +0600

    Categories JSON created

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./Data/categories.json');
 
 app.get('/', (req, res) => {
     res.send('Dream furniture API Running');
 });
+
+app.get('/products-categories', (req, res) =>{
+    res.send(categories)
+})
 
 app.listen(port, () => {
     console.log('Dream Furniture Server running on port', port)
