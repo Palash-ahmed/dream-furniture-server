a477c01859b1b8e7ca1b36889ace419d4e167509
Palash Ahmed <palashbiochem@gmail.com>
2022-11-25 12:43:03 +0600

    git ignore

--- index.js
+++ index.js
@@ -9,13 +9,26 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.selxuid.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const categoriesCollection = client.db('furnix').collection('products');
+
+        app.get('/products', async(req, res)=>{
+            const query = {}
+            const cursor = categoriesCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(error => console.error(error));
 
 const categories = require('./Data/categories.json');
 const products = require('./Data/products.json');
