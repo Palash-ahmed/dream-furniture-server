4dc690aed13d915228e2125d20a364ada8ece092
Palash Ahmed <palashbiochem@gmail.com>
2022-11-27 01:03:22 +0600

    json web token done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,14 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.selxuid.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.send(401).send('unauthorized access');
+    }
+    const token = authHeader.split(' ')[1];
+}
 
 async function run(){
     try{
@@ -33,9 +42,9 @@ async function run(){
             res.send(products_Collection);
         });  
 
-        app.get('/bookings', async(req, res)=>{
+        app.get('/bookings', verifyJWT, async(req, res)=>{
             const email = req.query.email;
-            const query = {buyerEmail: email};
+            const query = {email: email};
             const orders = await bookingsCollection.find(query).toArray();
             res.send(orders);
         });
@@ -45,6 +54,17 @@ async function run(){
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         });
+
+        app.get('/jwt', async(req, res)=>{
+            const email = req.query.email;
+            const query = {email: email};
+            const user =await usersCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'});
+                return res.send({accessToken: token});
+            }
+            res.status(403).send({accessToken: ''})
+        })
 
         app.post('/users', async(req, res)=>{
             const user = req.body;
