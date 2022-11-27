0f6c6629759a167b85276d8bc8288fdfb7b2a84f
Palash Ahmed <palashbiochem@gmail.com>
2022-11-28 00:58:59 +0600

    new product created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
@@ -13,67 +13,131 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function verifyJWT(req, res, next){
+function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.send(401).send('unauthorized access');
+    if (!authHeader) {
+        return res.status(401).send('Unauthorized Access');
     }
     const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (error, decoded) {
+        if (error) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
 }
 
-async function run(){
-    try{
+async function run() {
+    try {
         const categoriesCollection = client.db('furnix').collection('categories');
         const productsCollection = client.db('furnix').collection('products');
         const bookingsCollection = client.db('furnix').collection('bookings');
         const usersCollection = client.db('furnix').collection('users');
 
-        app.get('/categories', async(req, res)=>{
+        app.get('/categories', async (req, res) => {
             const query = {}
             const category = await categoriesCollection.find(query).toArray();
             res.send(category);
         });
 
-        app.get('/categories/:id', async(req, res) =>{
+        app.get('/categories/:id', async (req, res) => {
             const id = req.params.id;
             const query = {}
             const products = await productsCollection.find(query).toArray();
             const products_Collection = products.filter(product => product.category_id === id);
             res.send(products_Collection);
-        });  
+        });
 
-        app.get('/bookings', verifyJWT, async(req, res)=>{
+        app.get('/categoriesNewProduct', async(req, res)=>{
+            const query = {}
+            const result = await categoriesCollection.find(query).project({id: 1}).toArray();
+            res.send(result);
+        });
+
+        app.post('/products', async(req, res) =>{
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result);
+        })
+
+        app.get('/bookings', verifyJWT, async (req, res) => {
             const email = req.query.email;
-            const query = {email: email};
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'Forbidden Access' });
+            }
+            const query = { email: email };
             const orders = await bookingsCollection.find(query).toArray();
             res.send(orders);
         });
 
-        app.post('/bookings', async(req, res)=>{
+        app.post('/bookings', async (req, res) => {
             const booking = req.body
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         });
 
-        app.get('/jwt', async(req, res)=>{
+        app.get('/jwt', async (req, res) => {
             const email = req.query.email;
-            const query = {email: email};
-            const user =await usersCollection.findOne(query);
-            if(user){
-                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'});
-                return res.send({accessToken: token});
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN);
+                return res.send({ accessToken: token });
             }
-            res.status(403).send({accessToken: ''})
+            res.status(403).send({ accessToken: '' })
+        });
+
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+
+
+        // =============================
+        // user admin
+        // =============================
+
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+
         })
 
-        app.post('/users', async(req, res)=>{
+        app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollection.findOne(query);
+
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ message: 'Forbidden Access' })
+            }
+
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         })
 
     }
-    finally{
+    finally {
 
     }
 }
@@ -82,7 +146,7 @@ run().catch(error => console.error(error));
 
 
 
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     res.send('Dream furniture API is Running');
 });
 
